7033b89f6f646ab6de2dbf7584e1589405123eaf
zhuyie <zhuyie@gmail.com>
2021-12-31 17:38:29 +0800

    Replace qsufsort with divsufsort

--- source/bsdiff.c
+++ source/bsdiff.c
@@ -30,7 +30,10 @@
 #include <string.h>
 #include <stdint.h>
 #include <sys/types.h>
+
 #include <bzlib.h>
+#include <divsufsort.h>
+#include <divsufsort64.h>
 
 #include "bsdiff.h"
 #include "misc.h"
@@ -40,134 +43,6 @@
 #endif
 
 #define MIN(x,y) (((x)<(y)) ? (x) : (y))
-
-static void split(int64_t *I, int64_t *V, int64_t start, int64_t len, int64_t h)
-{
-	int64_t i, j, k, x, tmp, jj, kk;
-
-	if (len < 16) {
-		for (k = start; k < start + len; k += j) {
-			j = 1; x = V[I[k] + h];
-			for (i = 1; k + i < start + len; i++) {
-				if (V[I[k+i] + h] < x) {
-					x = V[I[k+i] + h];
-					j = 0;
-				};
-				if (V[I[k+i] + h] == x) {
-					tmp = I[k+j];
-					I[k+j] = I[k+i];
-					I[k+i] = tmp;
-					j++;
-				};
-			};
-			for (i = 0; i < j; i++)
-				V[I[k+i]] = k+j-1;
-			if (j == 1)
-				I[k] = -1;
-		};
-		return;
-	};
-
-	x = V[I[start+len/2]+h];
-	jj=0; kk=0;
-	for (i = start; i < start+len; i++) {
-		if (V[I[i]+h] < x)
-			jj++;
-		if (V[I[i]+h] == x)
-			kk++;
-	}
-	jj += start;
-	kk += jj;
-
-	i = start; j=0; k=0;
-	while (i < jj) {
-		if (V[I[i]+h] < x) {
-			i++;
-		} else if(V[I[i]+h] == x) {
-			tmp = I[i];
-			I[i] = I[jj+j];
-			I[jj+j] = tmp;
-			j++;
-		} else {
-			tmp = I[i];
-			I[i] = I[kk+k];
-			I[kk+k] = tmp;
-			k++;
-		};
-	};
-
-	while (jj+j<kk) {
-		if (V[I[jj+j]+h] == x) {
-			j++;
-		} else {
-			tmp = I[jj+j];
-			I[jj+j] = I[kk+k];
-			I[kk+k] = tmp;
-			k++;
-		};
-	};
-
-	if (jj > start)
-		split(I, V, start, jj-start, h);
-
-	for (i = 0; i < kk-jj; i++)
-		V[I[jj+i]] = kk-1;
-	if (jj == kk-1)
-		I[jj] = -1;
-
-	if (start+len > kk)
-		split(I, V, kk, start+len-kk, h);
-}
-
-static void qsufsort(int64_t *I, int64_t *V, uint8_t *old, int64_t oldsize)
-{
-	int64_t buckets[256];
-	int64_t i,h,len;
-
-	for (i = 0; i < 256; i++)
-		buckets[i] = 0;
-	for (i = 0; i < oldsize; i++)
-		buckets[old[i]]++;
-	for (i = 1; i < 256; i++)
-		buckets[i] += buckets[i-1];
-	for (i = 255; i > 0; i--)
-		buckets[i] = buckets[i-1];
-	buckets[0] = 0;
-
-	for (i = 0; i < oldsize; i++)
-		I[++buckets[old[i]]] = i;
-	I[0] = oldsize;
-	for (i = 0; i < oldsize; i++)
-		V[i] = buckets[old[i]];
-	V[oldsize] = 0;
-	for (i = 1; i < 256; i++) {
-		if (buckets[i] == buckets[i - 1] + 1)
-			I[buckets[i]] = -1;
-	}
-	I[0] = -1;
-
-	for (h = 1; I[0] != -(oldsize+1); h += h) {
-		len = 0;
-		for (i = 0; i < oldsize+1;) {
-			if (I[i] < 0) {
-				len -= I[i];
-				i -= I[i];
-			} else {
-				if (len)
-					I[i-len] = -len;
-				len = V[I[i]]+1 - i;
-				split(I, V, i, len, h);
-				i += len;
-				len = 0;
-			};
-		};
-		if (len)
-			I[i-len] = -len;
-	};
-
-	for (i = 0; i < oldsize+1; i++)
-		I[V[i]] = i;
-}
 
 static int64_t matchlen(uint8_t *old, int64_t oldsize, uint8_t *new, int64_t newsize)
 {
@@ -180,29 +55,29 @@ static int64_t matchlen(uint8_t *old, int64_t oldsize, uint8_t *new, int64_t new
 	return i;
 }
 
-static int64_t search(int64_t *I, uint8_t *old, int64_t oldsize,
+static int64_t search64(int64_t *SA, uint8_t *old, int64_t oldsize,
 		uint8_t *new, int64_t newsize, int64_t st, int64_t en, int64_t *pos)
 {
 	int64_t x, y;
 
 	if (en-st < 2) {
-		x = matchlen(old+I[st], oldsize-I[st], new, newsize);
-		y = matchlen(old+I[en], oldsize-I[en], new, newsize);
+		x = matchlen(old+SA[st], oldsize-SA[st], new, newsize);
+		y = matchlen(old+SA[en], oldsize-SA[en], new, newsize);
 
 		if (x > y) {
-			*pos = I[st];
+			*pos = SA[st];
 			return x;
 		} else {
-			*pos = I[en];
+			*pos = SA[en];
 			return y;
 		}
 	};
 
 	x = st+(en-st)/2;
-	if (memcmp(old+I[x], new, (size_t)MIN(oldsize-I[x], newsize)) < 0) {
-		return search(I, old, oldsize, new, newsize, x, en, pos);
+	if (memcmp(old+SA[x], new, (size_t)MIN(oldsize-SA[x], newsize)) < 0) {
+		return search64(SA, old, oldsize, new, newsize, x, en, pos);
 	} else {
-		return search(I, old, oldsize, new, newsize, st, x, pos);
+		return search64(SA, old, oldsize, new, newsize, st, x, pos);
 	};
 }
 
@@ -237,7 +112,7 @@ int bsdiff(
 	int ret;
 	uint8_t *old = NULL, *new = NULL;
 	int64_t oldsize, newsize, patchsize, patchsize2;
-	int64_t *I = NULL, *V = NULL;
+	int64_t *SA = NULL;
 	int64_t scan, pos, len;
 	int64_t lastscan, lastpos, lastoffset;
 	int64_t oldscore, scsc;
@@ -266,13 +141,12 @@ int bsdiff(
 		HANDLE_ERROR(BSDIFF_FILE_ERROR, "read oldfile");
 
 	cb = (size_t)((oldsize + 1) * sizeof(int64_t));
-	if (((I = malloc(cb)) == NULL) || ((V = malloc(cb)) == NULL))
-		HANDLE_ERROR(BSDIFF_OUT_OF_MEMORY, "malloc for I && V");
+	if ((SA = malloc(cb)) == NULL)
+		HANDLE_ERROR(BSDIFF_OUT_OF_MEMORY, "malloc for SA");
 
-	qsufsort(I, V, old, oldsize);
-
-	free(V);
-	V = NULL;
+	SA[0] = oldsize;
+	if (divsufsort64(old, SA + 1, oldsize) != 0)
+		HANDLE_ERROR(BSDIFF_ERROR, "constructs suffix array");
 
 	/* Allocate newsize+1 bytes instead of newsize bytes to ensure
 		that we never try to malloc(0) and get a NULL pointer */
@@ -327,7 +201,7 @@ int bsdiff(
 		oldscore = 0;
 
 		for (scsc = scan+=len; scan < newsize; scan++) {
-			len = search(I, old, oldsize, new+scan, newsize-scan,
+			len = search64(SA, old, oldsize, new+scan, newsize-scan,
 					0, oldsize, &pos);
 
 			for (; scsc < scan + len; scsc++) {
@@ -471,7 +345,7 @@ cleanup:
 	bsdiff_close_compressor(&pfbz2);
 	if (db != NULL) { free(db); }
 	if (eb != NULL) { free(eb); }
-	if (I != NULL) { free(I); }
+	if (SA != NULL) { free(SA); }
 	if (old != NULL) { free(old); }
 	if (new != NULL) { free(new); }
 
